19eb1773be5879738148f1e3137877f8972117b1
Cole Cunningham <cunningham.cole@outlook.com>
2017-08-14 16:03:00 -0400

    handled infinite scrolling

--- games.py
+++ games.py
@@ -1,18 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
+def createSoup(username, page):
+    r = requests.get("https://psnprofiles.com/{}?ajax=1&completion=all&order=last-played&pf=all&page={}".format(username,page), 'html.parser')
+    response=r.json()['html']
+    soup=BeautifulSoup(response, 'html.parser')
+    return soup
+
 username = input("Enter username. ")
-url = "https://psnprofiles.com/"+username
-r = requests.get(url, 'html.parser')
+page = 1
 
-soup = BeautifulSoup(r.content, 'html.parser')
+soup = createSoup(username, page)
+result = soup.find(text="No games found")
 
-games = soup.find('table', id="gamesTable").find_all("tr")
-for game in games:
-        numTrophiesEarned = int(game.find("span", class_="small-info").find("b").text)
-        if numTrophiesEarned > 0:
-            print(game.find("a", class_="title").text)
-            #print(game.find("span", class_="small-info").find("b").text)
-            
-	
-input("Press Enter to continue...")
+if result is None:
+    games = soup.find_all('tr')
+    for game in games:
+        try:
+            numTrophiesEarned = int(game.find("span", class_="small-info").find("b").text)
+            if numTrophiesEarned > 0:
+                print(game.find("a", class_="title").text)
+        except:
+            pass
+    page = page + 1
+    soup = createSoup(username, page)
+    result = soup.find(text="No games found")
+
+
